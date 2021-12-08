a6d4b52f57d50e624da0e7aca321a4c6f07e3c51
es5h playground <lspk.lpg@gmail.com>
2021-12-08 23:28:26 +0900

    대충 수정

--- src/ProblemAbstractAndInterface/q03/TalkableTest.java
+++ src/ProblemAbstractAndInterface/q03/TalkableTest.java
@@ -1,27 +1,33 @@
 package ProblemAbstractAndInterface.q03;
 
 interface Talkable {
-    void talk();
+
+	void talk();
 }
+
 class Korean implements Talkable {
-    public void talk() {
-        System.out.println("안녕하세요! 만나서 반갑습니다.");
-    }
+
+	public void talk() {
+		System.out.println("안녕하세요! 만나서 반갑습니다.");
+	}
 }
 
 class English implements Talkable {
-    public void talk() {
-        System.out.println("Hello. Nice to meet you!");
-    }
+
+	public void talk() {
+		System.out.println("Hello. Nice to meet you!");
+	}
 }
 
 public class TalkableTest {
-    static void speak(Talkable language){
-        language.talk();
-    }
-    public static void main(String[] args) {
-        speak(new Korean());
-        speak(new English());
-    }
+
+	private static void speak(Talkable language) {
+		language.talk();
+	}
+
+	public static void main(String[] args) {
+		speak(new Korean());
+		speak(new English());
+	}
 
 }
--- src/ProblemAbstractAndInterface/q06/HotelTest.java
+++ src/ProblemAbstractAndInterface/q06/HotelTest.java
@@ -2,46 +2,63 @@ package ProblemAbstractAndInterface.q06;
 
 class Hotel {
 
-    private static class Room {
+	private static class Room {
 
-        int number;
-        String client;
+		private final Integer number;
+		private final String client;
 
-        public Room(int number, String client) {
-            this.number = number;
-            this.client = client;
-        }
-    }
+		public Room(Integer number, String client) {
+			this.number = number;
+			this.client = client;
+		}
+	}
 
-    final private Room[] rooms = new Room[10];
+	private final Room[] rooms = new Room[10];
 
-    public void add(int number, String client) {
-        number = Math.max(0, Math.min(9, number));
-        if (rooms[number] == null) {
-            rooms[number] = new Room(number, client);
-        }
-    }
+	public Integer numberBound(Integer number) {
+		if (number < 0) {
+			return 0;
+		}
+		if (number > 9) {
+			return 9;
+		}
 
-    public void show() {
-        for (Room room : rooms) {
-            if (room != null) {
-                System.out.println((room.number) + 1 + " 번 방을 " + room.client + "님께서 예약하셨습니다.");
-            }
+		return number;
+	}
 
-        }
-    }
+	private boolean isEmpty(Room room) {
+		return room == null;
+	}
+
+	public void add(Integer number, String client) {
+		number = numberBound(number);
+		Room room = new Room(number, client);
+
+		if (isEmpty(rooms[number])) {
+			rooms[number] = room;
+		}
+	}
+
+	public void show() {
+		for (Room room : rooms) {
+			if (room != null) {
+				System.out.println((room.number) + 1 + " 번 방을 " + room.client + "님께서 예약하셨습니다.");
+			}
+
+		}
+	}
 }
 
 
 public class HotelTest {
 
-    public static void main(String[] args) {
-        Hotel hotel = new Hotel();
-        hotel.add(11, "호돌이");
-        hotel.add(5, "RootSong");
-        hotel.add(5, "룻송");
-        hotel.add(7, "길동이");
-        hotel.show();
-    }
-
+	public static void main(String[] args) {
+		Hotel hotel = new Hotel();
+		hotel.add(13, "호돌이");
+		hotel.add(11, "호돌이");
+		hotel.add(5, "RootSong");
+		hotel.add(5, "룻송");
+		hotel.add(7, "길동이");
+		hotel.show();
+	}
 }
